3821c71feb45a5e9eca931b9bba062c9074188f6
Valentinas <valentinas.kaminskas@mif.stud.vu.lt>
2018-06-02 21:14:11 +0300

    From excel to picture functions added

--- MathOCRLibrary/DataGenerator/DataGenerator.py
+++ MathOCRLibrary/DataGenerator/DataGenerator.py
@@ -1,15 +1,22 @@
 import os
 from xlwt import Workbook, easyxf
 from traceback import format_exc
+from win32com.client import Dispatch
+from PIL import ImageGrab # to make a shortcut in excel
+from pywintypes import com_error
 
 directory = os.path.dirname (os.path.abspath (__file__))
 
 def getListFromFile (fileName):
+    """ Des: Function to create a list of lines in file
+        Inf: Valentinas                                 6/2/2018 """
     with open (os.path.join (directory, fileName), "r") as f:
         return [line for line in f.readlines ()]
             
 
 def printSymbolsToExcel (sheet, fontType = ""):
+    """ Des: Fill excel sheet with symbols and all different fonts
+        Inf: Valentinas, Deividas                        6/2/2018 """
     for i in range (0, len (symbolList)):
         sheet.col (i).width_mismatch = True
         sheet.col (i).width = 256 * 16
@@ -30,30 +37,84 @@ def printSymbolsToExcel (sheet, fontType = ""):
         line += 1 
 
 
-def SaveExcel (wb, fileName):
+def saveExcel (wb, fileName):
+    """ Des: Save excel file from started workbook
+        Inf: Valentinas                                 6/2/2018 """
     try:
-        wb.save (os.path.join (directory, fileName + ".xls"))
+        wb.save (os.path.join (directory, fileName))
         print (fileName + " was successfully created")
     except:
         print ("File is not created")
         print (format_exc ())
 
  
-def CreateAndSaveSheetInExcel (fileName, *styles):
+def createAndSaveSheetInExcel (fileName, *styles):
+    """ Des: Save excel file from started workbook
+        Inf: Valentinas                                 6/2/2018 """
     wb = Workbook ()
     fontSheet = wb.add_sheet (fileName)
     printSymbolsToExcel (fontSheet, ", ".join (styles))
-    SaveExcel (wb, fileName)
+    saveExcel (wb, fileName)
+
+
+def createAFolder (name):
+    """ Des: Create a folder in selected location
+        Inf: Valentinas                                 6/2/2018 """
+    path = os.path.join (directory, name)
+    if not os.path.exists (path):
+        os.makedirs (path)
+    return path
+
+
+def getImageFromCell (fileName):
+    """ Des: Get image from excel
+        Inf: Valentinas                                 6/2/2018 """
+    # TODO: need to test this function!!!
+    xls = Dispatch ('Excel.Application')
+    xlswb = xls.Workbooks.Open (os.path.join (directory, fileName), ReadOnly = True)
+    print (fileName[:-4])
+    excelFile = xlswb.Sheets (fileName[:-4])
+    for i in range (0, len (symbolList)):
+        for j in range (0, len (fontList)):
+            _range = "%s:%s" % (i, j)
+            try:
+                range = excelFile.workbook.Application.Range (_range)
+            except com_error:
+                raise Exception ("Failed locating range %s" % (_range))
+        
+            # See http://stackoverflow.com/a/42465354/1924207
+            for shape in range.parent.Shapes: pass
+
+            xlScreen, xlPrinter = 1, 2
+            xlPicture, xlBitmap = -4147, 2
+            retries, success = 50, False
+            while not success:
+                try:
+                    range.CopyPicture (xlScreen, xlBitmap)
+                    im = ImageGrab.grabclipboard ()
+                    im.save (str (symbolList[i]) + str (j) + fileName[:-4] + ".PNG", str (symbolList[i]) + str (j))
+                    success = True
+                except (com_error, AttributeError) as e:
+                    # When other (big) Excel documents are open,
+                    # copyPicture fails intermittently
+                    retries -= 1
+                    if retries == 0: raise
 
 
 fontList = getListFromFile ("Fonts.txt")
 symbolList = getListFromFile ("Symbols.txt")
 
 # Create and format an excel workbook for regular font
-CreateAndSaveSheetInExcel ("RegularFont")
+createAndSaveSheetInExcel ("RegularFont.xls")
 # Create and format an excel workbook for bold font
-CreateAndSaveSheetInExcel ("BoldFont", "bold 1")
+createAndSaveSheetInExcel ("BoldFont.xls", "bold 1")
 # Create and format an excel workbook for italic font
-CreateAndSaveSheetInExcel ("ItalicFont", "italic 1")
+createAndSaveSheetInExcel ("ItalicFont.xls", "italic 1")
 # Create and format an excel workbook for bold italic font
-CreateAndSaveSheetInExcel ("ItalicAndBoldFont", "bold 1", "italic 1")
+createAndSaveSheetInExcel ("ItalicAndBoldFont.xls", "bold 1", "italic 1")
+
+## create images from created excel files
+getImageFromCell ("RegularFont.xls")
+getImageFromCell ("BoldFont.xls")
+getImageFromCell ("ItalicFont.xls")
+getImageFromCell ("ItalicAndBoldFont.xls")
