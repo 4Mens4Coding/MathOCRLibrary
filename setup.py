d92cdefd4e8d3a0ad5217faab899220b909cf763
Valentinas <31697821+valentk777@users.noreply.github.com>
2018-06-08 17:06:28 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,17 +15,17 @@ REQUIRED = [
 ]
 
 setup(
-    name=NAME,
-    version=about['__version__'],
-    description=DESCRIPTION,
-    long_description=long_description,
-    long_description_content_type='text/markdown'
-    author=AUTHOR,
-    author_email=EMAIL,
-    python_requires=REQUIRES_PYTHON,
-    url=URL,
+    name = NAME,
+    version = about['__version__'],
+    description = DESCRIPTION,
+    long_description = long_description,
+    long_description_content_type = "text/markdown"
+    author = AUTHOR,
+    author_email = EMAIL,
+    python_requires = REQUIRES_PYTHON,
+    url = URL,
     packages = find_packages(),
-    install_requires=REQUIRED,
+    install_requires = REQUIRED,
     # install_requires = ['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
-    classifiers=["Programming Language :: Python :: 3.6"]
+    classifiers = ["Programming Language :: Python :: 3.6"]
 )
