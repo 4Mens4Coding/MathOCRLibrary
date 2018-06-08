558ef31fffdcd5130106586a69788259dffea6b3
Valentinas <31697821+valentk777@users.noreply.github.com>
2018-06-08 17:11:06 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,14 @@ REQUIRED = [
     # 'requests', 'maya', 'records',
 ]
 
+# Load the package's __version__.py module as a dictionary.
+about = {}
+if not VERSION:
+    with open(os.path.join(here, NAME, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
+
 setup(
     name = NAME,
     version = about['__version__'],
