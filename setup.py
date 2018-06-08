643cbced0536a384d547fbc9f8787fa20e7ea7c5
Valentinas <31697821+valentk777@users.noreply.github.com>
2018-06-08 16:54:48 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name = "MathOCRLibrary",
-    version = "1.0.0",
+    version = "1.0",
     url = "https://github.com/4Mens4Coding/MathOCRLibrary/tree/master/MathOCRLibrary",
-    author = "Author Name",
+    author = "valentk777, dogecode, tomas-drasutis, DeividasBrazenas",
     author_email = "valentk777@gmail.com",
     description = "Description of my package",
     packages = find_packages(),    
