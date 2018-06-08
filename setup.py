0b22ef440faafd8ede7998cdbee881a904a44f82
Valentinas <31697821+valentk777@users.noreply.github.com>
2018-06-08 17:05:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup, Command
+
+# Package meta-data.
+NAME = "MathOCRLibrary"
+DESCRIPTION = "Open source math OCR with ML"
+URL = "https://github.com/4Mens4Coding/MathOCRLibrary/tree/master/MathOCRLibrary"
+EMAIL = "valentk777@gmail.com"
+AUTHOR = "dogecode, tomas-drasutis, DeividasBrazenas, valentk777"
+REQUIRES_PYTHON = ">=3.6.0"
+VERSION = "1.0"
+
+# What packages are required for this module to be executed?
+REQUIRED = [
+    # 'requests', 'maya', 'records',
+]
 
 setup(
-    name = "MathOCRLibrary",
-    version = "1.0",
-    url = "https://github.com/4Mens4Coding/MathOCRLibrary/tree/master/MathOCRLibrary",
-    author = "valentk777, dogecode, tomas-drasutis, DeividasBrazenas",
-    author_email = "valentk777@gmail.com",
-    description = "Description of my package",
-    packages = find_packages(),    
+    name=NAME,
+    version=about['__version__'],
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+    author=AUTHOR,
+    author_email=EMAIL,
+    python_requires=REQUIRES_PYTHON,
+    url=URL,
+    packages = find_packages(),
+    install_requires=REQUIRED,
     # install_requires = ['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+    classifiers=["Programming Language :: Python :: 3.6"]
 )
