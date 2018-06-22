c555c68dfe8ef18c7f47f59ca0f33512a768ed8c
dogecade <vilcinskastomas911@gmail.com>
2018-06-22 04:24:05 -0400

    little tweaks to database access

--- MathOCRLibrary/ImageProcessing/DataController.py
+++ MathOCRLibrary/ImageProcessing/DataController.py
@@ -1,17 +1,15 @@
 from ImageProcessing import ImageController
-import MySQLdb
+import MySQLdb as mdb
  
-db = MySQLdb.connect (host = "localhost", user = "root", passwd = "50dbc998", db = "MathOCR")
- 
-cur = db.cursor()
- 
-ic = ImageController()
- 
-ic.appendImageFromPath("/home/MathOCR/MathOCRLibrary/ImageProcessing/6")
- 
-ic.firstToBinaryArray()
- 
-matrix = ic.getBinaryArray(0)
-correct = "5"
- 
-cur.execute("INSERT INTO binarizedData (image, correct) VALUES ({0},{1})".format(matrix,correct))
+try:
+  con = mdb.connect('176.223.133.57','valentk777','fbauisdvgasidugvalsduigas','MathOCR');
+  cur = con.cursor()
+  ic = ImageController() 
+  ic.appendImageFromPath("/home/MathOCR/MathOCRLibrary/ImageProcessing/6")
+  ic.firstToBinaryArray()
+  matrix = ic.getBinaryArray(0)
+  correct = "5"
+  cur.execute("INSERT INTO `MathOCR`.`binarizedData` (`id`,`image`,`correct`) VALUES ({0},{1},{2}".format(id,matrix,correct))
+finally:
+  if con:
+    con.close()
