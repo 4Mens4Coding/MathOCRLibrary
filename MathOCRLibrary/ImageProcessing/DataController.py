ab0bee061878676e56f8c0927236775b2d293128
valentk777 <valentk777@gmail.com>
2018-06-22 13:21:42 +0300

    Cosmetic Changes

--- MathOCRLibrary/ImageProcessing/DataController.py
+++ MathOCRLibrary/ImageProcessing/DataController.py
@@ -1,16 +1,16 @@
 from ImageProcessing import ImageController
-
 import MySQLdb as mdb
  
 try:
-  con = mdb.connect('server_ip','user','password','database');
-  cur = con.cursor()
-  ic = ImageController() 
-  ic.appendImageFromPath("/home/MathOCR/MathOCRLibrary/ImageProcessing/6")
-  ic.firstToBinaryArray()
-  matrix = ic.getBinaryArray(0)
-  correct = "5"
-  cur.execute("INSERT INTO `MathOCR`.`binarizedData` (`id`,`image`,`correct`) VALUES ({0},{1},{2}".format(id,matrix,correct))
+    con = mdb.connect ("server_ip", "user", "password", "database")
+    cur = con.cursor ()
+    ic = ImageController () 
+    ic.appendImageFromPath ("/home/MathOCR/MathOCRLibrary/ImageProcessing/6")
+    ic.firstToBinaryArray ()
+    matrix = ic.getBinaryArray (0)
+    correct = "5"
+    cur.execute ("INSERT INTO `MathOCR`.`binarizedData` (`id`,`image`,`correct`) VALUES ({0},{1},{2}".format (id, matrix, correct))
+
 finally:
-  if con:
-    con.close()
+    if (con):
+        con.close ()
